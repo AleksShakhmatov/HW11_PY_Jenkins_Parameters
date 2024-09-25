49f1e489a3d15c6a86d0f325cb80345bae4d8097
AleksSh <Coluchy@yandex.ru>
2024-09-26 00:45:11 +0600

    new code added

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,22 +1,10 @@
-
-import os
 import pytest
-from dotenv import load_dotenv
-from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
+from selene import browser
+import os
 from utils import attach
-
-
-DEFAULT_BROWSER_VERSION = "100.0"
-
-
-def pytest_addoption(parser):
-    parser.addoption(
-        '--browser_version',
-        default='100.0'
-    )
+from dotenv import load_dotenv
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -24,14 +12,12 @@ def load_env():
     load_dotenv()
 
 
-@pytest.fixture(scope='function')
-def setup_browser(request):
-    browser_version = request.config.getoption('--browser_version')
-    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
+@pytest.fixture(scope='function', autouse=True)
+def setup_browser():
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": browser_version,
+        "browserVersion": "100.0",
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
@@ -39,17 +25,18 @@ def setup_browser(request):
     }
     options.capabilities.update(selenoid_capabilities)
 
-    login = os.getenv('LOGIN')
-    password = os.getenv('PASSWORD')
+    login = os.getenv("LOGIN")
+    password = os.getenv("PASSWORD")
+    selenoid = os.getenv('SELENOID_URL')
 
     driver = webdriver.Remote(
-        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@{selenoid}/wd/hub",
         options=options
     )
-
-    browser.config.window_width = 1920
+    browser.config.base_url = "https://demoqa.com"
     browser.config.window_height = 1080
-    browser.config.base_url = 'https://demoqa.com/'
+    browser.config.window_width = 1920
+
     browser.config.driver = driver
     yield browser
 
